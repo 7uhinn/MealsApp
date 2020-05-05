c71e334590d97a0fce42f5b252a390baf9050263
Tuhin <tuhin_sarkar@srmap.edu.in>
2020-05-05 18:52:15 +0530

    screen basic

---
+++ lib/categoriesS.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './categoriesItemsW.dart';
+import './categoriesD.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MealsApp'),
+      ),
+      body: GridView(
+        children: dummy.map((ci) {
+          return CategoriesItems(ci.title, ci.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/categoriesM.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Categories{
+  String id;
+  String title;
+  Color color;
+
+  Categories({
+    this.id,
+    this.title,
+    this.color
+  });
+}
